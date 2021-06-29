ca92345dbef0de22282b7da1b52809c34c1e9563
Tazmondo <fondcoolman@outlook.com>
2021-06-29 19:11:55 +0100

    added grid, styling and basic functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+let generateGrid = function (pixelCount) {
+    let gridContainer = document.querySelector('#grid-container');
+    let pixelLength = gridContainer.clientWidth / pixelCount;
+    for(let i = 0; i < pixelCount*pixelCount; i++) {
+        let newDiv = document.createElement('div')
+        newDiv.style.width = pixelLength + "px";
+        newDiv.style.height = pixelLength + "px";
+        newDiv.classList.toggle('pixel');
+        newDiv.addEventListener("mouseenter", (ev => {
+            newDiv.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+        }));
+        gridContainer.appendChild(newDiv);
+    }
+}
+
+generateGrid(15);
